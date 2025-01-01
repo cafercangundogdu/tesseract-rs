95fd000e469b6b7b8a25e387e053cfe974028dba
Cafer Can Gündoğdu <cafercangundogdu@gmail.com>
2025-01-01 12:49:10 +0300

    feat(api): implement dynamic tessdata directory resolution based on OS

--- src/lib.rs
+++ src/lib.rs
@@ -12,11 +12,53 @@
 //! Here's a basic example of how to use `tesseract-rs`:
 //!
 //! ```rust
+//! use std::path::PathBuf;
 //! use tesseract_rs::TesseractAPI;
+//!
+//! fn get_default_tessdata_dir() -> PathBuf {
+//!     if cfg!(target_os = "macos") {
+//!         let home_dir = std::env::var("HOME").expect("HOME environment variable not set");
+//!         PathBuf::from(home_dir)
+//!             .join("Library")
+//!             .join("Application Support")
+//!             .join("tesseract-rs")
+//!             .join("tessdata")
+//!     } else if cfg!(target_os = "linux") {
+//!         let home_dir = std::env::var("HOME").expect("HOME environment variable not set");
+//!         PathBuf::from(home_dir)
+//!             .join(".tesseract-rs")
+//!             .join("tessdata")
+//!     } else if cfg!(target_os = "windows") {
+//!         PathBuf::from(std::env::var("APPDATA").expect("APPDATA environment variable not set"))
+//!             .join("tesseract-rs")
+//!             .join("tessdata")
+//!     } else {
+//!         panic!("Unsupported operating system");
+//!     }
+//! }
+//!
+//! fn get_tessdata_dir() -> PathBuf {
+//!     match std::env::var("TESSDATA_PREFIX") {
+//!         Ok(dir) => {
+//!             let path = PathBuf::from(dir);
+//!             println!("Using TESSDATA_PREFIX directory: {:?}", path);
+//!             path
+//!         }
+//!         Err(_) => {
+//!             let default_dir = get_default_tessdata_dir();
+//!             println!(
+//!                 "TESSDATA_PREFIX not set, using default directory: {:?}",
+//!                 default_dir
+//!             );
+//!             default_dir
+//!         }
+//!     }
+//! }
 //!
 //! fn main() -> Result<(), Box<dyn std::error::Error>> {
 //!     let api = TesseractAPI::new();
-//!     api.init("path/to/tessdata", "eng")?;
+//!     let tessdata_dir = get_tessdata_dir();
+//!     api.init(tessdata_dir.to_str().unwrap(), "eng")?;
 //!
 //!     // Assume we have a 3x3 black and white image with a "1"
 //!     let image_data: Vec<u8> = vec![
@@ -25,7 +67,7 @@
 //!         0x00, 0x00, 0xFF,
 //!     ];
 //!
-//!     api.set_image(&image_data, 3, 3, 1, 3);
+//!     api.set_image(&image_data, 3, 3, 1, 3)?;
 //!     api.set_variable("tessedit_char_whitelist", "0123456789")?;
 //!
 //!     let text = api.get_utf8_text()?;
